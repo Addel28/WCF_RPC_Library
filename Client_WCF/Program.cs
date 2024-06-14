1d7b4379eed8036939caaf94d7fa70558f17868a
Alex <mitrofanova21@yandex.ru>
2024-06-14 12:23:24 +0300

    Добавьте файлы проекта.

---
+++ Client_WCF/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCF_RPC_Library;
+
+namespace Client_WCF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var binding = new WSHttpBinding();
+            binding.Security.Mode = SecurityMode.Message;
+            binding.Security.Message.ClientCredentialType = MessageCredentialType.Windows;
+            var asyncFactory = new ChannelFactory<IOrderService>(binding, new EndpointAddress("http://localhost:8733/Design_Time_Addresses/WCF_RPC_Library/IOrderService/mx"));
+            //var asyncFactory = new ChannelFactory<IOrderService>(new BasicHttpBinding(), new EndpointAddress("http://localhost:8733/Design_Time_Addresses/WCF_RPC_Library/IOrderService/mx"));
+            var asyncClient = asyncFactory.CreateChannel();
+
+            // Асинхронный вызов клиента
+            Task.Run(async () =>
+            {
+                // Добавляем новый заказ
+                await asyncClient.AddOrderAsync(new Order
+                {
+                    OrderId = 2,
+                    CustomerName = "Jane Doe",
+                    OrderDate = DateTime.Now,
+                    Items = new List<OrderItem>
+                        {
+                            new OrderItem { ItemId = 1, ProductName = "Smartphone", Quantity = 1 },
+                            new OrderItem { ItemId = 2, ProductName = "Headphones", Quantity = 2 }
+                        }
+                });
+
+                // Получаем список всех заказов
+                var asyncOrders = await asyncClient.GetOrdersAsync();
+                foreach (var order in asyncOrders)
+                {
+                    Console.WriteLine($"Order ID: {order.OrderId}, Customer Name: {order.CustomerName}, Order Date: {order.OrderDate}");
+                }
+
+                // Получаем заказ по ID
+                var asyncSingleOrder = await asyncClient.GetOrderByIdAsync(2);
+                Console.WriteLine($"Single Order - ID: {asyncSingleOrder.OrderId}, Customer Name: {asyncSingleOrder.CustomerName}");
+            }).Wait();
+            Console.ReadLine();
+        } 
+    }
+}
+
---
+++ Service_WCF/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCF_RPC_Library;
+
+namespace Service_WCF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var hostTask = Task.Run(() =>
+            //{
+                using (ServiceHost host = new ServiceHost(typeof(OrderService), new Uri("http://localhost:8733/Design_Time_Addresses/WCF_RPC_Library/IOrderService/mx")))
+                {
+                    host.AddServiceEndpoint(typeof(IOrderService), new WSHttpBinding(), "");
+                    host.Open();
+                    Console.WriteLine("Sync Service is running...");
+                    Console.ReadLine();
+                    //using (ServiceHost asyncHost = new ServiceHost(typeof(AsyncOrderService), new Uri("http://localhost:8000/AsyncOrderService")))
+                    //{
+                    //    asyncHost.AddServiceEndpoint(typeof(IAsyncOrderService), new BasicHttpBinding(), "");
+                    //    asyncHost.Open();
+                    //    Console.WriteLine("Async Service is running...");
+                    //    Console.ReadLine();
+                    //}
+                }
+            //});
+        }
+    }
+}
